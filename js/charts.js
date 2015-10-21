37cff1a5c677f9c178e378662ecacb40586a3dba
Derek Eder <derek.eder@gmail.com>
2015-10-21 15:58:57 -0500

    started filling out sections

--- js/charts.js
+++ js/charts.js
@@ -1,85 +1,42 @@
 $(function () {
-    $('#top-mdb-chart').highcharts({
+    $('#chart-findings-1').highcharts({
         chart: {
-            type: 'column',
-            backgroundColor: null,
-            height: 200
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
         },
-        title:{
-            text: '',
-            margin: 0
-        },
-        legend: {
-            enabled: false
-        },
-        xAxis: {
-            categories: [   'Austin',
-                            'Humboldt Park',
-                            'North Lawndale',
-                            'West Englewood',
-                            'Roseland'
-                        ],
-            title: {
-                text: null
-            },
-            labels: {
-                style: {
-                    'font-family': '"Lato", sans-serif',
-                    'color': '#E8B19C',
-                    'font-size': '.85em'
-                }
-            },
-            lineWidth: 0,
-            tickWidth: 0
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: ''            },
-            labels: {
-                enabled: false
-            },
-            gridLineWidth: 0,
-            endOnTick: true
+        title: {
+            text: 'Browser market shares January, 2015 to May, 2015'
         },
         tooltip: {
-            valuePrefix: '$',
-            borderWidth: 0,
-            borderRadius: 0,
-            formatter: function() {
-                return '$'+commaSeparateNumber(this.y);
-            }
-        }, 
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
         plotOptions: {
-            column: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
                 dataLabels: {
                     enabled: true,
-                    //format: '${y}',
-                    formatter: function () {
-                        var s = '$<strong>' + Math.round(this.y / 1000000) + '</strong>m';
-
-                        return s;
-                    },
-                    color: '#E8B19C',
-                    inside: true,
-                    verticalAlign: 'top',
-                    style:{
-                        'font-family': '"Lato", sans-serif'
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
                     }
-                },
-                borderWidth: 0,
-                groupPadding: 0
-            },
-            series: {
-                color: '#A50F15'
+                }
             }
         },
-        credits: {
-            enabled: false
-        },
         series: [{
-            name: 'Spending',
-            data: [550234592, 292506638, 240683262, 197168151, 159211446]
+            name: "Brands",
+            colorByPoint: true,
+            data: [{
+                name: "No punishment",
+                y: 95.0
+            }, {
+                name: "Punishment",
+                y: 5.0,
+                sliced: true,
+                selected: true
+            }]
         }]
     });
 });
